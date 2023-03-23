e804d95b9b0a1199117074ba5e0dffdbe525671f
oliverlabs <70239916+oliverlabs@users.noreply.github.com>
2023-03-23 16:27:08 +0000

    added a masspublish powershell script

---
+++ utilities/pipelines/resourcePublish/Publish-AllModules.ps1
@@ -0,0 +1,6 @@
+﻿$modules = Get-ChildItem -Path '<FullPath>\CARML\ResourceModules\modules' -Recurse -Filter 'deploy.bicep'
+
+$modules.FullName | ForEach-Object -Parallel {
+    . '<FullPath>\CARML\ResourceModules\utilities\pipelines\resourcePublish\Publish-ModuleToPrivateBicepRegistry.ps1'
+    Publish-ModuleToPrivateBicepRegistry -TemplateFilePath $_ -ModuleVersion '<version>' -BicepRegistryName '<brName>' -BicepRegistryRgName '<rgName>'
+} -ThrottleLimit 4
